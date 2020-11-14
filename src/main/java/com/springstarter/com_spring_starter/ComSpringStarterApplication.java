b201d1aae6e3c8ad831749cb73686a515b79d5fb
DEVIN KEO <devinkeo@DEVINs-MacBook-Pro.local>
2020-11-14 17:08:23 +0700

    init project

---
+++ src/main/java/com/springstarter/com_spring_starter/ComSpringStarterApplication.java
@@ -0,0 +1,13 @@
+package com.springstarter.com_spring_starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComSpringStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComSpringStarterApplication.class, args);
+    }
+
+}
